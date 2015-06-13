19b2b51fd123c24b87a0bf1b4f7a8633ce06c552
ramakant1985 <timehasnoholidays@gmail.com>
2015-06-13 12:07:12 +0000

    chef and recipes associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150613111225) do
+
+  create_table "chefs", force: true do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "chef_id"
+  end
 
 end
